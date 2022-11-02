8ab2b97555477b97db8df6493f6bac62268a01de
Iqballlfw <iqbalfardurahman@gmail.com>
2022-11-02 07:24:25 +0700

    debug

--- MODUL2 FADLI/FADLI_mybooking.php
+++ MODUL2 FADLI/FADLI_mybooking.php
@@ -105,10 +105,10 @@
           <tbody>
             <tr>
               <th><?php echo rand(0, 99999999) ?></th>
-              <th><?php echo $name; ?></th>
+              <th><?php echo $nama; ?></th>
               <th><?php echo $tanggal." ".$waktu ; ?></th>
               <th><?php echo $checkout." ".$waktu; ?></th>
-              <th><?php echo $pilihMobil; ?></th>
+              <th><?php echo $pilihMobils; ?></th>
               <th><?php echo $number; ?></th>
               <th>
                 <ul>
